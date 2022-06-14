92e6b03fbfdbed08a99707f8225e68b27cdc98ce
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-06-14 11:58:12 -0500

    Se carga multiples imagenes a firebase.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -6,21 +6,26 @@ from django.core.files.storage import FileSystemStorage
 
 from main.settings import BASE_DIR
 
-# Create your views here.
-
 
 class UserProfile(TemplateView):
     bucket = storage.bucket()
     template_name = 'pages/user_profile/form.html'
 
     def post(self, request, *args, **kwargs):
-        img = request.FILES.get('imagen')
-        correo = request.POST.get('correo')
-        fss = FileSystemStorage()
-        file = fss.save(img.name, img)
+        email = request.POST.get('email')
+        imgs = request.FILES.getlist('images')
+        imgs_urls = []
 
-        blob = self.bucket.blob(f'images/{file}')
-        blob.upload_from_filename(BASE_DIR/file)
-        fss.delete(file)
-        blob.make_public()
-        return render(request, self.template_name, {'url': blob.public_url})
+        print(f'EMAIL: {email}')
+
+        for img in imgs:
+            fss = FileSystemStorage()
+            file = fss.save(img.name, img)
+
+            blob = self.bucket.blob(f'images/{file}')
+            blob.upload_from_filename(BASE_DIR/file)
+            fss.delete(file)
+            blob.make_public()
+            imgs_urls.append(blob.public_url)
+
+        return render(request, self.template_name, {'urls': imgs_urls})
